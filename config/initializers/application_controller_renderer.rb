c73c8d46002d676af8162faea378eb4ce1278d44
nawaru817 <t010mbharada@gmail.com>
2021-02-15 07:50:59 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c3004f249051e44a30f60c55af5608cb585a3bd30dc46f8c66410ee2713c7351f02ccb0bfdd98268533500d81e8c6e2791d07e6ce48d72119f73a1fea83dc22b'
